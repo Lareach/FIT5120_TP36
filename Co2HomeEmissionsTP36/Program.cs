d07579de238e373b04e7d06cf31953832f1a225c
rantad584 <rantad584@github.com>
2024-03-30 22:31:38 +1100

    Set up connection strings for local and hosting databases

--- Co2HomeEmissionsTP36/Program.cs
+++ Co2HomeEmissionsTP36/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add DbContext to the container.
+builder.Services.AddDbContext<Co2HomeEmissionsTP36.Data.SavingsContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
