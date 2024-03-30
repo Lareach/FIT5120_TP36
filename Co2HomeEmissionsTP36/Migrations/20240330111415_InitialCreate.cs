2fe562d354348562e3bb644cb8b42f6abbcd45a8
rantad584 <rantad584@github.com>
2024-03-30 22:34:11 +1100

    Set up migrations to automate creating database schema

---
+++ Co2HomeEmissionsTP36/Migrations/20240330111415_InitialCreate.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Co2HomeEmissionsTP36.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "category",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    Uid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_category", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "concession",
+                columns: table => new
+                {
+                    ConcessionId = table.Column<int>(type: "int", nullable: false),
+                    Uid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ConcessionName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_concession", x => x.ConcessionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "savings",
+                columns: table => new
+                {
+                    SavingsId = table.Column<int>(type: "int", nullable: false),
+                    Uid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Method = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Duration = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    EligibilityRequirements = table.Column<string>(type: "nvarchar(600)", maxLength: 600, nullable: true),
+                    CtaUrl = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    ConcessionId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_savings", x => x.SavingsId);
+                    table.ForeignKey(
+                        name: "FK_savings_category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "category",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_savings_concession_ConcessionId",
+                        column: x => x.ConcessionId,
+                        principalTable: "concession",
+                        principalColumn: "ConcessionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_savings_CategoryId",
+                table: "savings",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_savings_ConcessionId",
+                table: "savings",
+                column: "ConcessionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "savings");
+
+            migrationBuilder.DropTable(
+                name: "category");
+
+            migrationBuilder.DropTable(
+                name: "concession");
+        }
+    }
+}
