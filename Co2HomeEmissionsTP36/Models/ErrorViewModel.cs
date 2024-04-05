254a4dd8359e347ff39a68445b0bdb745f6b8922
Lareach <lareach123@gmail.com>
2024-04-05 22:27:43 +1100

    remove old files

--- Co2HomeEmissionsTP36/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Co2HomeEmissionsTP36.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
