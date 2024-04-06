911208a82bed5c359eb9e96c9bc1d7ff290de3b7
rantad584 <rantad584@github.com>
2024-04-06 14:38:16 +1100

    Display question in questionnaire one at a time

--- Co2HomeEmissionsTP36/wwwroot/js/site.js
+++ Co2HomeEmissionsTP36/wwwroot/js/site.js
@@ -1,4 +1,53 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿jQuery(document).ready(function($) {
 
-// Write your JavaScript code.
+    var currentQuestion = 1;
+
+    // Initially disable the next button
+    $("#nextButton").prop("disabled", true);
+
+    // Check if at least one option is selected before enabling the next button
+    $(".question").each(function() {
+        $(this).find(":input").change(function() {
+            var isValid = $(this).closest(".question").find(":input:checked").length > 0;
+            $("#nextButton").prop("disabled", !isValid);
+        });
+    });
+
+    // Next button click event
+    $("#nextButton").click(function() {
+        $("#question" + currentQuestion).hide();
+        currentQuestion++;
+        if (currentQuestion <= 4) {
+            $("#question" + currentQuestion).show();
+            $("#backButton").show();
+        }
+        if (currentQuestion === 4) {
+            $("#nextButton").hide();
+            $("#submitButton").show();
+        }
+        // Disable next button if no option is selected
+        $("#nextButton").prop("disabled", true);
+        
+        if ($("#question" + currentQuestion).find(":input:checked").length > 0) {
+            $("#nextButton").prop("disabled", false);
+        }
+    });
+
+    // Back button click event
+    $("#backButton").click(function() {
+        $("#question" + currentQuestion).hide();
+        currentQuestion--;
+        if (currentQuestion >= 1) {
+            $("#question" + currentQuestion).show();
+            if (currentQuestion === 1) {
+                $("#backButton").hide();
+            }
+            if (currentQuestion < 4) {
+                $("#nextButton").show();
+                $("#submitButton").hide();
+            }
+        }
+
+        $("#nextButton").prop("disabled", false);
+    });
+});
