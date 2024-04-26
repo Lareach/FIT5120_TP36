1bfb853b17735fb4039ec3dff5c12ff5a2dc33c8
Lareach <lareach123@gmail.com>
2024-04-26 10:10:00 +1000

    3d model implememtation iteration 2 last edit

---
+++ FIT5120_TP36-main/FIT5120_TP36-main/Co2HomeEmissionsTP36/wwwroot/js/nodeData.js
@@ -0,0 +1,75 @@
+﻿var nodeData = {
+    "solarPanels": {
+        title: "Solar Panels",
+        description: "<p><strong>Money-Saving Tips:</strong></p>" +
+            "<ul>" +
+            "<li>Rebates and interest-free loans are now available to eligible households to replace their solar PV system if it is over ten years old.</li>" +
+            "<li>Existing customers who received a rebate at their old address and have moved house can now apply for multiple incentives for their new property if it is their primary residence.</li>" +
+            "<li>Eligible property owners with a home under construction can apply for a solar panel (PV) rebate as soon as they move in.</li>" +
+            "<li>Eligible rental providers can apply for a solar rebate for their rental property, saving on installation costs and reducing energy bills for renters.</li>" +
+            "</ul>" +
+            "<p><strong>About:</strong></p>" +
+            "<p>Installing a solar electricity system in Victoria offers several financial benefits, including rebates for solar photovoltaic (PV) systems, hot water systems, solar batteries, and interest-free loans. Proper planning and installation, guided by resources like the ‘Buyers Guide developed with Renew’, are crucial for maximising energy and cost savings. Considerations include system size, component quality, and choosing a reputable installer. Rebates enhance affordability, and understanding grid connection is important for effective system integration. Warranties and consumer protections provide additional security and confidence in the investment. <a href='https://www.solar.vic.gov.au/solar-panel-rebate' target='_blank'>Learn more>></a></p>" +
+            "<p><strong>Governmental Support:</strong></p>" +
+            "<a href='https://www.solar.vic.gov.au/solar-panel-rebate' target='_blank'>Solar Panel Rebate</a>"
+        ,
+
+        imageUrl: "https://example.com/image_solar_panels.jpg"
+    }
+    ,
+    "outdoorlighting": {
+        title: "Outdoor Solar Lighting",
+        description: "<p><strong>Money-Saving Tips:</strong></p>" +
+            "<ul>" +
+            "<li>Solar panels can be used to provide energy to internal lighting. Outdoors, solar lights can be used to light walkways, porches, pools, fountains, and other outdoor features with little to no CO2 emissions.</li>" +
+            "<li>Passive solar is a no-cost lighting solution. Passive solar means letting the natural sunlight your home. Whether it’s through strategically placed windows, skylights, or solar tubes, the sun can do a tremendous job of lighting your home for free.</li>" +
+            "</ul>" +
+            "<p><strong>About:</strong></p>" +
+            "<p>Switching to solar lighting in Australia is highly beneficial for environmental and economic reasons. Solar lights are a sustainable choice, reducing carbon footprints and electricity costs. They're easy to install, require minimal maintenance, and can be used in various locations, including remote areas without power grids. Additionally, solar lights are safe, enhance property security, and support wildlife.</p>" +
+            "<p><strong>Governmental Support:</strong></p>" +
+            "<a href='https://www.solar.vic.gov.au/solar-rebates' target='_blank'>Solar Rebates</a>"
+        ,
+        imageUrl: "https://example.com/image_outdoor_lighting.jpg"
+    },
+    "evcharger": {
+        title: "EV Charger",
+        description: "<p><strong>Money-Saving Tips:</strong></p>" +
+            "<ul>" +
+            "<li>Install Solar Panels: The initial installation cost is high, but it's a cost-effective method to charge an electric car. Solar panels generate free, clean energy, reducing overall home energy expenses.</li>" +
+            "<li>Charge During Off-Peak Periods: Charging costs are lower during off-peak hours, typically at night. Chargers can be programmed to avoid peak hours, enhancing savings.</li>" +
+            "<li>Avoid Rapid Charging: Although rapid charging is convenient, it can be very expensive.</li>" +
+            "<li>Utilise Government Schemes: Incentives are available to reduce the costs of electric vehicle adoption. Grants cover up to 75% of the cost of installing a home charging wall box.</li>" +
+            "</ul>" +
+            "<p><strong>About:</strong></p>" +
+            "<p>Charging an electric vehicle (EV) at home in Melbourne involves various options and considerations:</p>" +
+            "<ul>" +
+            "<li>Charging Options: Level 1 uses standard outlets for slow charging; Level 2 requires a charging station to be installed for faster, overnight charging.</li>" +
+            "<li>Installation: A licensed electrician should install Level 2 stations, adhering to Australian Standards (AS/NZS 3000) for safety.</li>" +
+            "<li>Costs: Costs include purchasing the charger and installation. Running costs vary by electricity tariff, with time-of-use tariffs offering cheaper night-time charging.</li>" +
+            "<li>Government Incentives: Victorian Government incentives may help offset installation costs.</li>" +
+            "<li>Future Planning: Consider potential future needs like charging multiple EVs or upgrading chargers as EV usage grows.</li>" +
+            "</ul>" +
+            "<p><strong>Governmental Support:</strong></p>" +
+            "<a href='https://www.solar.vic.gov.au/getting-your-home-ev-ready' target='_blank'>Getting Your Home EV Ready</a>"
+        ,
+        imageUrl: "https://example.com/image_ev_charger.jpg"
+    },
+    "heatwaterPump": {
+        title: "Heat Water Pump",
+        description: "<p><strong>Money-Saving Tips:</strong></p>" +
+            "<ul>" +
+            "<li>If you’re going to be away from your house for an extended period, switch the heat-pump water heater off at the switchboard to save energy.</li>" +
+            "<li>Every degree above 20 degrees can add 10% to your heating bill. In winter, heating can account for over 30% of your bill.</li>" +
+            "</ul>" +
+            "<p><strong>About:</strong></p>" +
+            "<p>Heat-pump water heaters extract heat from the surrounding air to heat water, using about 60-75% less electricity than conventional electric heaters.</p>" +
+            "<p>They function like reverse-cycle air conditioners, using electricity for the heat pump, not for directly heating water.</p>" +
+            "<p>Available as integrated units or split systems. They require proper ventilation and consideration for noise, especially near bedrooms.</p>" +
+            "<p>Suitable for off-peak electricity tariffs, these systems can also utilise timers to align with solar photovoltaic systems for efficient energy use.</p>" +
+            "<p>Eligible for government rebates in some regions, they are efficient in warmer climates but may have slower reheat rates in cold weather.</p>" +
+            "<p><strong>Governmental Support:</strong></p>" +
+            "<a href='https://www.energy.vic.gov.au/households/victorian-energy-upgrades-for-households/heating-and-cooling' target='_blank'>Victorian Energy Upgrades for Households</a>"
+        ,
+        imageUrl: "https://example.com/image_heat_pump.jpg"
+    }
+};
